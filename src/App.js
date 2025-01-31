e9305e47bdc7c64cf8c467ec7b528735482dabb4
Marco03 <jack.owl03m@gmail.com>
2025-01-31 15:35:22 +0100

    update css, add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import logo from './logo.svg';
 import gear from './gear.png';
 import './App.css';
+import './styles/navbar.css'; // Assicurati che il percorso del CSS sia corretto
+import Home from './pages/Home';   // Importazione dalla cartella 'pages'
+import About from './pages/About'; // Importazione dalla cartella 'pages'
+import Resume from './pages/Resume'; // Importazione dalla cartella 'pages'
+import Navbar from './compontents/Navbar'; // Importazione del componente Navbar
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={gear} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Helmet>
+          <title>Test</title>
+        </Helmet>
+        <header className="App-header">
+          {/* Barra di navigazione */}
+          <Navbar /> {/* Usa il componente Navbar qui */}
+          <Routes>
+            {/* Rotta di default */}
+            <Route path="/my-page/" element={<Home />} />
+            <Route path="/my-page/about" element={<About />} />
+            <Route path="/my-page/resume" element={<Resume />} />
+            
+            {/* Redirect esplicito alla home, se la rotta non esiste */}
+            <Route path="*" element={<Navigate to="/my-page" replace />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
