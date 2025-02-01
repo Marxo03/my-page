b6403faae075c07e6a58af5034a2cf6888d0c7e2
Marco03 <jack.owl03m@gmail.com>
2025-02-01 15:16:21 +0100

    navbar hidden menu fixed

--- src/compontents/Navbar.jsx
+++ src/compontents/Navbar.jsx
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/navbar.css';
-import logo from '../assets/my-logo_transparent.png'; // Importa l'immagine
+import logo from '../assets/my-logo_transparent.png';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Funzione per chiudere il menù al click di un link
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav>
       <div className="logo">
-        <img src={logo} alt="Logo" /> {/* Aggiungi l'immagine */}
-      </div>      
-      <ul>
+        <img src={logo} alt="Logo" />
+        <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        {isMenuOpen ? <i className='bx bx-x'></i> : <i className='bx bx-menu'></i>}
+      </div>
+      </div>
+      
+      <ul className={`nav-links ${isMenuOpen ? 'show' : ''}`}>
         <li>
-          <NavLink to="/my-page" end activeClassName="active">
+          <NavLink to="/my-page" end activeClassName="active" onClick={closeMenu}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/about" activeClassName="active">
+          <NavLink to="/my-page/about" activeClassName="active" onClick={closeMenu}>
             About
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/resume" activeClassName="active">
+          <NavLink to="/my-page/resume" activeClassName="active" onClick={closeMenu}>
             Resume
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/projects" activeClassName="active">
+          <NavLink to="/my-page/projects" activeClassName="active" onClick={closeMenu}>
             Projects
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/contacts" activeClassName="active">
+          <NavLink to="/my-page/contacts" activeClassName="active" onClick={closeMenu}>
             Contacts
           </NavLink>
         </li>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -103,28 +103,55 @@ function Resume() {
         <div className="content">
           <div className="top">
             <div className="logo-details">
-              <div className="logo">
-                <NavLink to="/my-page" end activeClassName="active">
-                  <img src={logo} alt="Logo" className="footer-logo" />
-                </NavLink>
-              </div> 
+            <div className="logo">
+              <NavLink to="/my-page" end activeClassName="active">
+                <img src={logo} alt="Logo" className="footer-logo" />
+              </NavLink>
+            </div> 
             </div>
             <div className="media-icons">
               <a href="https://github.com/Marxo03"><i class='bx bxl-github'></i></a>
               <a href="https://discord.com/users/j4ck_owl03"><i class='bx bxl-discord-alt'></i></a>
-              <a href="https://twitter.com/myprofile"><i class='bx bxl-twitter'></i></a>
-              <a href="https://instagram.com/myprofile"><i class='bx bxl-instagram'></i></a>
-              <a href="mailto:jack.owl03m@gmail.com"><i class='bx bxl-gmail'></i></a>
+              <a href="https://twitter.com/myprofile"><i class='bx bxl-twitter' ></i></a>
+              <a href="https://instagram.com/myprofile"><i class='bx bxl-instagram' ></i></a>
+              <a href="mailto:jack.owl03m@gmail.com"><i class='bx bxl-gmail' ></i></a>
             </div>
           </div>
 
           <div className="link-boxes">
             <ul className="box">
               <li className="link_name">Sections</li>
-              <li><NavLink to="/my-page" end activeClassName="active">Home</NavLink></li>
-              <li><NavLink to="/my-page/about" activeClassName="active">About</NavLink></li>
-              <li><NavLink to="/my-page/resume" activeClassName="active">Resume</NavLink></li>
-              <li><NavLink to="/my-page/projects" activeClassName="active">Projects</NavLink></li>
+              <li>
+                <NavLink to="/my-page" end activeClassName="active">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/my-page/about" activeClassName="active">
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/my-page/resume" activeClassName="active">
+                  Resume
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/my-page/projects" activeClassName="active">
+                  Projects
+                </NavLink>
+                </li>
+            </ul>
+            <ul className="box">
+              <li className="link_name">My Projects</li>
+              <li><a href="#arduino">Arduino Projects</a></li>
+              <li><a href="https://technomances.github.io/">Health4Paws</a></li>
+              <li><a href="#ecopath">EcoPath</a></li>
+              <li><a href="#another">Other Projects</a></li>
+            </ul>
+            <ul className="box">
+              <li className="link_name">Contact</li>
+              <li><a href="mailto:jack.owl03m@gmail.com">jack.owl03m@gmail.com</a></li>
             </ul>
           </div>
         </div>
@@ -134,7 +161,7 @@ function Resume() {
             <span className="copyright_text"><a href="https://opensource.org/license/mit"> &copy; Copyright 2025 Marxo03</a></span>
             <span className="policy_terms">
               <a href="#privacy-policy">Privacy policy</a>
-              <div className="credit">Made with <span style={{ color: 'tomato' }}><i class='bx bxs-heart'></i></span> by <a href="https://www.marxo03.com/">Marxo03</a></div>
+              <div className="credit">Made with <span style={{ color: 'tomato' }}><i class='bx bxs-heart' ></i></span> by <a href="https://www.marxo03.com/">Marxo03</a></div>
             </span>
           </div>
         </div>
