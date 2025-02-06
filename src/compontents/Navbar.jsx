dd9f4a2516aedc1dd2588ab1e3a740ac1778ff5a
Marco03 <jack.owl03m@gmail.com>
2025-02-06 12:01:32 +0100

    mp3 updates

--- src/compontents/Navbar.jsx
+++ src/compontents/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/navbar.css';
 import logo from '../assets/my-logo_transparent.png';
+import clickSound from '../assets/pop-sound.mp3';  // Importa il file audio
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -20,6 +21,13 @@ const Navbar = () => {
     window.scrollTo(0, 0); // Scrolla la pagina all'inizio
   };
 
+  // Funzione per riprodurre il suono al click
+  const playSound = () => {
+    const audio = new Audio(clickSound); // Crea un oggetto audio
+    audio.playbackRate = 1.5; // Aumenta la velocità (1.5 è più veloce, puoi provarne altri valori)
+    audio.play(); // Riproduce il suono
+  };
+  
   return (
     <nav>
       <div className="logo">
@@ -31,27 +39,48 @@ const Navbar = () => {
       
       <ul className={`nav-links ${isMenuOpen ? 'show' : ''}`}>
         <li>
-          <NavLink to="/my-page" end activeClassName="active" onClick={() => { closeMenu(); scrollToTop(); }}>
+          <NavLink
+            to="/my-page"
+            end
+            activeClassName="active"
+            onClick={() => { closeMenu(); scrollToTop(); playSound(); }}
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/about" activeClassName="active" onClick={() => { closeMenu(); scrollToTop(); }}>
+          <NavLink
+            to="/my-page/about"
+            activeClassName="active"
+            onClick={() => { closeMenu(); scrollToTop(); playSound(); }}
+          >
             About
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/resume" activeClassName="active" onClick={() => { closeMenu(); scrollToTop(); }}>
+          <NavLink
+            to="/my-page/resume"
+            activeClassName="active"
+            onClick={() => { closeMenu(); scrollToTop(); playSound(); }}
+          >
             Resume
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/projects" activeClassName="active" onClick={() => { closeMenu(); scrollToTop(); }}>
+          <NavLink
+            to="/my-page/projects"
+            activeClassName="active"
+            onClick={() => { closeMenu(); scrollToTop(); playSound(); }}
+          >
             Projects
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-page/contacts" activeClassName="active" onClick={() => { closeMenu(); scrollToTop(); }}>
+          <NavLink
+            to="/my-page/contacts"
+            activeClassName="active"
+            onClick={() => { closeMenu(); scrollToTop(); playSound(); }}
+          >
             Contacts
           </NavLink>
         </li>
